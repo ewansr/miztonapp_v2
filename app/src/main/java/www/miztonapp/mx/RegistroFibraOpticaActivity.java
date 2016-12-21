8959416eb824a4381ddb64bed1248746f0d579d6
ewansr <ewansr@gmail.com>
2016-12-21 02:21:20 -0600

    vaidacion de un campo

--- app/src/main/java/www/miztonapp/mx/RegistroFibraOpticaActivity.java
+++ app/src/main/java/www/miztonapp/mx/RegistroFibraOpticaActivity.java
@@ -5,32 +5,61 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.rengwuxian.materialedittext.validation.RegexpValidator;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
-public class RegistroFibraOpticaActivity extends AppCompatActivity {
+import www.miztonapp.mx.utilerias.Utils;
+
+public class RegistroFibraOpticaActivity extends AppCompatActivity implements View.OnClickListener {
+    private MaterialEditText edtDistrito, edtFolio, edtTelefono, edtTerminal, edtPuerto;
+    private Button btnGuardar;
+
+    private static final String[] ESTATUS = new String[] {
+            "Liquidada", "Objetada", "Queja", "Retornada"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_fibra);
 
-        MaterialEditText edtDistrito = (MaterialEditText) findViewById(R.id.edtDistrito);
+        edtDistrito = (MaterialEditText) findViewById(R.id.edtDistrito);
         edtDistrito.addValidator(new RegexpValidator("Formato inválido 'ABC1234'", "[A-Z^\\s][A-Z^\\s][A-Z^\\s]\\d\\d\\d+"));
 
-        MaterialEditText edtFolio = (MaterialEditText) findViewById(R.id.edtFolio);
+        edtFolio = (MaterialEditText) findViewById(R.id.edtFolio);
         edtFolio.addValidator(new RegexpValidator("Formato inválido '123456789'", "\\d\\d\\d\\d\\d\\d\\d\\d\\d"));
 
-        MaterialEditText edtTelefono = (MaterialEditText) findViewById(R.id.edtTelefono);
+        edtTelefono = (MaterialEditText) findViewById(R.id.edtTelefono);
         edtTelefono.addValidator(new RegexpValidator("Formato inválido '0123456789'", "\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d"));
 
-        MaterialEditText edtTerminal = (MaterialEditText) findViewById(R.id.edtTerminal);
+        edtTerminal = (MaterialEditText) findViewById(R.id.edtTerminal);
         edtTerminal.addValidator(new RegexpValidator("Formato inválido 'A1'", "[A-Z^\\s]\\d"));
 
-        MaterialEditText edtPuerto = (MaterialEditText) findViewById(R.id.edtPuerto);
+        edtPuerto = (MaterialEditText) findViewById(R.id.edtPuerto);
         edtPuerto.addValidator(new RegexpValidator("Formato inválido '1-9'", "\\d"));
 
+        btnGuardar = (Button) findViewById(R.id.btnGuardar);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,  ESTATUS);
+        MaterialBetterSpinner textView = (MaterialBetterSpinner) findViewById(R.id.sp_estatus);
+        textView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnGuardar){
+            if (validaCampos()) {
+                Utils.crear_alerta(this,"no se puede guardar","Campos con formato inválidos o vacios");
+            }
+        }
+    }
+
+    private Boolean validaCampos(){
+        return edtFolio.isValid("\\d\\d\\d\\d\\d\\d\\d\\d\\d");
     }
 }
